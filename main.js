e1e4c64b5c1985fdce7014008d402aa069479b9b
Tanmoy037 <tanmoysantra67@gmail.com>
2024-03-22 23:39:16 +0530

    handeling api using promise and try catch

--- main.js
+++ main.js
@@ -51,12 +51,15 @@
 
 //// promise
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response) => {
-    return response.json()
-})
-.then ((data) => {
-    console.log(data)
-})
-.catch((error) => console.log(error))
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response) => {
+//     return response.json()
+// })
+// .then ((data) => {
+//     console.log(data)
+// })
+// .catch((error) => console.log(error))
+
+
+
 
