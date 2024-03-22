bb7c212df3a9006826b0992d3f60a91b47370a1f
Tanmoy037 <tanmoysantra67@gmail.com>
2024-03-22 22:41:53 +0530

    promise added

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ console.log("Hello");
 ((tea) => {
     console.log(`Take your ${tea}`);
 })("american tea")
+
+
+// promise
+
+const promiseOne = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = false;
+        if (!error) {
+            resolve({username: "Tanmoy", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+
+    }, 1000);
+}).then(user => {
+    console.log(user.username);
+    // Handle success scenario
+}).catch(error => {
+    console.error(error);
+    // Handle error scenario
+})
+
+
